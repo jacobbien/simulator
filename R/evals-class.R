d9d4ee4523b2a3f7b819a30d3bb43abfd27fcaaa
jbien <jbien@Jacobs-MacBook-Pro.local>
2015-10-11 03:00:02 -0400

    Developed Evals further - load_evals can combine across multiple methods now (rather than returning a list of Evals objects, one per method).  To do this, structure of Evals object is different from before (it now is of form object@evals$method_name$ri.j$metric_name) - subset_evals can perform subsetting both on metrics and on methods - as.data.frame for Evals converts Evals object to data.frame

--- R/evals-class.R
+++ R/evals-class.R
@@ -1,15 +1,34 @@
 check_evals <- function(object) {
   errors <- is_valid_name(object@model_name, "model_name")
-  errors <- c(errors, is_valid_name(object@method_name, "method_name"))
+  errors <- c(errors, is_valid_name(object@method_name, "method_name",
+                                    require_unique = FALSE))
   errors <- c(errors, is_valid_name(object@metric_name, "metric_name",
                                     require_unique = FALSE))
   if (length(object@index) < 1)
     errors <- c(errors, "index must be of length >= 1.")
   else if (any(object@index != round(object@index)))
     errors <- c(errors, "index must be an integer-valued numeric.")
+  if (length(object@method_label) != length(object@method_name))
+    errors <- c(errors, "method_label must have same length as method_name.")
   if (length(object@metric_label) != length(object@metric_name))
     errors <- c(errors, "metric_label must have same length as metric_name.")
-  errors <- c(errors, is_valid_rij_list(object@evals, object@index))
+  if (length(object@evals) != length(object@method_name)) {
+    errors <- c(errors, "length of object@evals must be same as method_name")
+    if (any(names(object@evals) != object@method_name))
+      errors <- c(errors, "names(object@evals) must match method_name")
+  }
+  for (m in seq_along(object@method_name)) {
+    errors <- c(errors, is_valid_rij_list(object@evals[[m]], object@index))
+    for (r in seq_along(object@evals[[m]])) {
+      nams <- names(object@evals[[m]][[r]])
+      if (length(nams) != length(object@metric_name))
+        errors <- c(errors, paste0("length of metric_name does not match ",
+                                   "length of object@evals[[m]][[r]]"))
+      if (any(nams != object@metric_name))
+        errors <- c(errors, paste0("metric_name does not match ",
+                                   "names(object@evals[[m]][[r]])"))
+    }
+  }
   if (length(errors) == 0) TRUE else errors
 }
 
@@ -36,10 +55,12 @@ check_evals <- function(object) {
 #'       derived from.
 #' @slot metric_label the label of the \code{\link{Metric}} object this output
 #'       is derived from.
-#' @slot evals a named list with each element labeled as \code{ri.j} where
-#'       \code{i} is the \code{index} and \code{j} ranges from \code{1} to
-#'       \code{nsim}.  Element \code{out$ri.j} is output of metric
-#'       \code{metric_name} on random draw \code{ri.j}.
+#' @slot evals a named list with each element labeled by a method_name
+#'       each evals[[m]] is itself a named list with each element labeled
+#'       as \code{ri.j} where \code{i} is the \code{index} and \code{j} ranges
+#'       from \code{1} to \code{nsim}.  Element \code{out$ri.j} is output of
+#'       metric \code{metric_name} on random draw \code{ri.j}.
+#' @seealso \code{\link{evaluate}} \code{\link{as.data.frame.Evals}}
 #' @export
 setClass("Evals", representation(model_name = "character",
                                  model_label = "character",
@@ -56,12 +77,47 @@ setMethod("show", "Evals", function(object) {
   cat(paste0("Evals Component"), fill = TRUE)
   cat(sprintf(" model_name: %s    index: %s (%s nsim%s)", object@model_name,
               paste(object@index, collapse = ", "),
-              length(object@evals), ifelse(length(object@index) > 1,
+              length(object@evals[[1]]), ifelse(length(object@index) > 1,
                                            " total", "")), fill = TRUE)
-  cat(sprintf(" method_name: %s (label: %s)", object@method_name,
-              object@method_label), fill = TRUE)
+  cat(sprintf(" method_name(s): %s (labeled: %s)",
+              paste(object@method_name, collapse = ", "),
+              paste(object@method_label, collapse = ", ")), fill = TRUE)
   cat(paste0(" metric_name(s): ", paste(object@metric_name, collapse = ", ")),
       fill = TRUE)
   cat(paste0(" metric_label(s): ", paste(object@metric_label, collapse = ", ")),
       fill = TRUE)
 })
+
+#' Convert an Evals to a data.frame
+#'
+#' This is equivalent to calling \code{as(x, "data.frame")}
+#' @param x object of class \code{\link{Evals}}
+#' @export
+as.data.frame.Evals = function(x) as(x, "data.frame")
+
+setAs(from = "Evals", to = "data.frame",
+      def = function(from) {
+        num_methods <- length(from@method_name)
+        for (m in seq(num_methods)) {
+          list_of_df <- lapply(from@evals[[m]], as.data.frame)
+          num_sim <- length(from@evals[[m]])
+          name_of_draw <- names(from@evals[[m]])
+          dfm <- cbind(Draw = name_of_draw[1], list_of_df[[1]])
+          if (num_sim > 1) {
+            for (r in seq(2, num_sim)) {
+              dfm <- rbind(dfm,
+                          cbind(Draw = name_of_draw[r], list_of_df[[r]]))
+            }
+          }
+          dfm <- cbind(Model = from@model_name,
+                       Method = from@method_name[m],
+                       dfm)
+          if (m == 1)
+            df <- dfm
+          else
+            df <- rbind(df, dfm)
+        }
+        names(df)[-(1:3)] <- from@metric_name
+        df
+        })
+
--- R/evaluate.R
+++ R/evaluate.R
@@ -73,15 +73,16 @@ evaluate_single <- function(metrics, model, output) {
   stopifnot(class(model) == "Model")
   stopifnot(class(output) == "Output")
   evals <- list()
+  evals[[output@method_name]] <- list()
+  metric_names <- unlist(lapply(metrics, function(m) m@name))
+  metric_labels <- unlist(lapply(metrics, function(m) m@label))
   for (rid in names(output@out)) {
-    evals[[rid]] <- list()
+    evals[[1]][[rid]] <- list()
     for (m in seq_along(metrics)) {
-      evals[[rid]][[metrics[[m]]@name]] <- metrics[[m]]@metric(model,
+      evals[[1]][[rid]][[metric_names[m]]] <- metrics[[m]]@metric(model@params,
                                                              output@out[[rid]])
     }
   }
-  metric_names <- unlist(lapply(metrics, function(m) m@name))
-  metric_labels <- unlist(lapply(metrics, function(m) m@label))
   new("Evals", model_name = model@name,
                model_label = model@label,
                index = output@index,
@@ -104,57 +105,57 @@ evaluate_single <- function(metrics, model, output) {
 #' @param dir the directory passed to \code{\link{generate_model}})
 #' @param model_name the \code{\link{Model}} object's \code{name}
 #' @param index a vector of positive integers.
-#' @param method_name the \code{name} of one or more \code{\link{Method}}
+#' @param method_names the \code{name} of one or more \code{\link{Method}}
 #'        objects.
 #' @param metric_names (optional) a character vector of which elements of
 #'        evals should be loaded. If NULL, then all elements are loaded.
 #' @param out_loc only needed if it was used in call to
 #'        \code{\link{run_method}}.
-#' @seealso \code{\link{run_method}} \code{\link{load_model}} \code{\link{load_draws}}
+#' @seealso \code{\link{load_model}} \code{\link{load_draws}} \code{\link{as.data.frame.Evals}}
 #' @examples
 #' \dontrun{
 #' }
-load_evals <- function(dir, model_name, index, method_name,
+load_evals <- function(dir, model_name, index, method_names,
                          metric_names = NULL, out_loc = "out") {
   md <- get_model_dir_and_file(dir, model_name)
   index <- sort(unique(index))
-  num_methods <- length(method_name)
-  if (!is.null(metric_names)) metric_names <- sort(metric_names)
+  num_methods <- length(method_names)
+  method_names <- sort(method_names)
+  method_labels <- rep(NA, num_methods)
   out_dir <- file.path(md$dir, remove_slash(out_loc))
-  evals_list <- list() # will have length num_methods
-  if (length(index) == 1) {
-    evals_files <- sprintf("%s/r%s_%s_evals.Rdata", out_dir, index,
-                           method_name)
-    for (m in seq(num_methods)) {
-      tryCatch(load(evals_files[m]),
-               warning=function(w)
-                 stop(sprintf("Could not find output file at %s.",
-                              evals_files[m])))
-      if (!is.null(metric_names))
-        evals <- subset_evals(evals, metric_names)
-      evals_list[[m]] <- evals
-    }
-    ifelse(num_methods == 1, return(evals_list[[1]]), return(evals_list))
-  }
+  newevals <- list()
   for (m in seq(num_methods)) {
-    newevals <- list()
+    newevals[[method_names[m]]] <- list()
     evals_files <- sprintf("%s/r%s_%s_evals.Rdata", out_dir, index,
-                           method_name[m])
+                           method_names[m])
     for (i in seq_along(index)) {
       tryCatch(load(evals_files[i]),
                warning=function(w)
                  stop(sprintf("Could not find evals file at %s.",
                               evals_files[i])))
       if (!is.null(metric_names)) {
-        evals <- subset_evals(evals, metric_names)
+        evals <- subset_evals(evals, metric_names = metric_names)
       }
-      newevals <- c(newevals, evals@evals)
+      if (m > 1) {
+        this_metric_names <- names(evals@evals[[1]][[1]])
+        prev_metric_names <- names(newevals[[m - 1]][[1]])
+        if (!all(this_metric_names == prev_metric_names))
+          stop("Cannot create Evals object with methods having different sets",
+               " of metrics computed.  Not all methods have ",
+               paste(setdiff(union(this_metric_names, prev_metric_names),
+                             intersect(this_metric_names, prev_metric_names)),
+                     collapse = ", "),
+               " computed.")
+      }
+      newevals[[m]] <- c(newevals[[m]], evals@evals[[1]])
     }
-    evals@index <- index
-    evals@evals <- newevals
-    evals_list[[m]] <- evals
+    method_labels[m] <- evals@method_label
   }
-  ifelse(num_methods == 1, return(evals_list[[1]]), return(evals_list))
+  evals@index <- index
+  evals@method_name <- method_names
+  evals@method_label <- method_labels
+  evals@evals <- newevals
+  return(evals)
 }
 
 
@@ -167,16 +168,32 @@ save_evals_to_file <- function(out_dir, evals) {
   file
 }
 
-
-
-subset_evals <- function(evals, metric_names) {
-  for (j in seq(length(evals@evals))) {
-    if (!(all(metric_names %in% names(evals@evals[[j]]))))
-      stop("Element ", names(evals@evals)[j], " does not match metric_names.")
-    evals@evals[[j]] <- evals@evals[[j]][metric_names]
+#' Reduce an Evals object to a subset of methods and/or metrics
+#'
+#' If \code{method_names} is \code{NULL}, then subsetting is not done over
+#' methods.  Likewise for \code{metric_names}.
+#'
+#' @param evals an object of class \code{\link{Evals}}.
+#' @param method_names a character vector of method names
+#' @param metric_names a character vector of metric names
+#' @export
+subset_evals <- function(evals, method_names = NULL, metric_names = NULL) {
+  if (!is.null(method_names)) {
+    stopifnot(method_names %in% evals@method_name)
+    ii <- match(method_names, evals@method_name)
+    evals@method_name <- evals@method_name[ii]
+    evals@method_label <- evals@method_label[ii]
+    evals@evals <- evals@evals[evals@method_name]
   }
-  ii <- match(metric_names, evals@metric_name)
-  evals@metric_name <- evals@metric_name[ii]
-  evals@metric_label <- evals@metric_label[ii]
+  if (!is.null(metric_names)) {
+    stopifnot(metric_names %in% evals@metric_name)
+    ii <- match(metric_names, evals@metric_name)
+    evals@metric_name <- evals@metric_name[ii]
+    evals@metric_label <- evals@metric_label[ii]
+    for (m in names(evals@evals))
+      for (r in names(evals@evals[[m]])) {
+        evals@evals[[m]][[r]] <- evals@evals[[m]][[r]][evals@metric_name]
+      }
+  }
   evals
 }
