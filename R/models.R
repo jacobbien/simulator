110eac2b296d7e29e86c4ebec0585859a0a73df4
jbien <jbien@Jacobs-MacBook-Pro.local>
2015-10-17 20:35:34 -0400

    Allow generate_model to form multiple models by varying one or more parameters

--- R/models.R
+++ R/models.R
@@ -4,10 +4,18 @@ NULL
 #' Generate a model.
 #'
 #' This function executes the \code{make_model} function provided by the user
-#' and writes to file the resulting \code{\link{Model}} object.  For example,
+#' and writes to file the resulting \code{\link{Model}} object(s).  For example,
 #' when simulating regression with a fixed design, \code{X} would be generated
 #' in this function and \code{n}, \code{p}, \code{beta}, and \code{sigma} would
 #' also be specified.
+#'
+#' When \code{make_model} has arguments, these can be passed using \code{...}.
+#' These will be passed directly to \code{make_model} except for any arguments
+#' named in \code{vary_along}.  These arguments should be lists and a separate
+#' model will be created for each combination of elements in these lists.  For
+#' example, if \code{vary_along = c("n", "p")}, then we can pass
+#' \code{n=as.list(c(50, 100, 150))} and \code{p=as.list(c(10, 100))} and 6
+#' models will be created, one for each pair of \code{n} and \code{p}.
 #'
 #' \code{make_model} is called generating an object of class
 #' \code{\link{Model}}, called \code{model}, which is saved to
@@ -23,13 +31,14 @@ NULL
 #'        save \code{\link{Model}} object in. Default is current working
 #'        directory
 #' @param seed an integer seed for the random number generator.
+#' @param vary_along character vector with all elements contained in names(...)
+#'        See description for more details.
 #' @param ... optional parameters that may be passed to make_model
 #' @seealso \code{\link{simulate_from_model}} \code{\link{run_method}}
 #' @examples
 #' \dontrun{
-#'  make_my_model <- function() {
+#'  make_my_model <- function(n = 10) {
 #'     # this function returns an object of class Model
-#'     n <- 20
 #'     params <- list(n = n, mu = rnorm(n))
 #'     simulate <- function(mu, n, nsim) {
 #'       # define function here that returns a list of length nsim
@@ -41,20 +50,63 @@ NULL
 #'                params = params, simulate = simulate))
 #'  }
 #'  generate_model(make_my_model, dir = ".")
+#'  generate_model(make_my_model, dir = ".", n = 20)
+#'  generate_model(make_my_model, dir = ".", n = as.list(c(10, 20, 30)),
+#'                 vary_along = "n")
 #'  }
-generate_model <- function(make_model, dir = ".", seed=123, ...) {
-  # Thinking of this as an internal function.  See MakeFixed.
+generate_model <- function(make_model, dir = ".", seed = 123, vary_along = NULL, ...) {
   stopifnot(class(make_model) == "function")
   dir <- remove_slash(dir)
   stopifnot(file.info(dir)$isdir)
+  passed_params <- as.list(match.call(expand.dots = FALSE)$`...`)
+  passed_params <- lapply(passed_params, eval)
+  if (is.null(vary_along))
+    return(generate_model_single(make_model, dir, seed, passed_params))
+  stopifnot(is.character(vary_along))
+  if (!all(vary_along %in% names(passed_params)))
+    stop("vary_along must only include names of parameters passed via \"...\".")
+  if (!all(unlist(lapply(passed_params[vary_along], is.list))))
+      stop("each parameter named in vary_along must be passed (through ...)",
+           " as a list.")
+  indices <- lapply(passed_params[vary_along], function(a) seq(length(a)))
+  ii <- expand.grid(indices)
+  # loop over all combinations of parameters named in vary_along
+  params_to_pass <- passed_params
+  for (i in seq(nrow(ii))) {
+    for (j in seq(ncol(ii))) { # jth vary_along parameter
+      var <- vary_along[j]
+      params_to_pass[[var]] <- passed_params[[var]][[ii[i, j]]]
+    }
+    extension <- paste(vary_along, ii[i, ], sep = "", collapse = "/")
+    generate_model_single(make_model, dir, seed, params_to_pass, extension)
+  }
+}
+
+generate_model_single <- function(make_model, dir, seed, params_to_pass,
+                                  extension = NULL) {
   # initialize lecuyer RNG:
   RNGkind("L'Ecuyer-CMRG")
   set.seed(seed)
   rng_seed <- .Random.seed # this is the seed used when Model is generated
   # generate model object:
-  model <- make_model(...)
+  if (is.null(params_to_pass))
+    model <- make_model()
+  else
+    model <- do.call(make_model, params_to_pass)
   if (class(model) != "Model")
     stop("make_model must return an object of class Model.")
+  # if any passed parameters are added to model@params by make_model, make sure
+  # they match the values passed (to avoid a potentially hard to find bug)
+  added_already <- intersect(names(params_to_pass), names(model@params))
+  for (param in added_already) {
+    if (!identical(model@params[[param]], params_to_pass[[param]]))
+      stop("make_model sets ", param, " to a value different from value",
+           " passed to it by generate_model.")
+  }
+  to_add <- setdiff(names(params_to_pass), names(model@params))
+  model@params <- c(model@params, params_to_pass[to_add])
+  if (!is.null(extension))
+    model@name <- sprintf("%s/%s", model@name, extension)
   # create directories files and files/model_name if don't exist)
   files_dir <- file.path(dir, getOption("simulator.files"))
   model_dir <- file.path(files_dir, model@name)
@@ -68,7 +120,6 @@ generate_model <- function(make_model, dir = ".", seed=123, ...) {
   catsim("..Created model and saved in", file, fill = TRUE)
   invisible(file)
 }
-
 
 #' Load a model from file.
 #'
---
+++ tests/testthat/test-models.R
@@ -0,0 +1,71 @@
+options(simulator.verbose = FALSE)
+
+context("generate_model")
+
+make_regmodel <- function(n, p, sigma) {
+  x <- matrix(rnorm(n * p), n, p)
+  beta <- runif(p)
+  return(new("Model", name = "reg",
+             label = sprintf("Regression model"),
+             params = list(x = x, beta = beta, signal = x %*% beta,
+                           n = 1, sigma = sigma),
+             simulate = function(signal, n, sigma, nsim) {
+               y <- list()
+               for (i in seq(nsim))
+                 y[[i]] <- signal + sigma * rnorm(n)
+               return(y)
+             }))
+}
+
+make_regmodel2 <- function(n, p, sigma) {
+  x <- matrix(rnorm(n * p), n, p)
+  beta <- runif(p)
+  return(new("Model", name = "reg2",
+             label = sprintf("Regression model"),
+             params = list(x = x, beta = beta, signal = x %*% beta,
+                           n = n, sigma = sigma),
+             simulate = function(signal, n, sigma, nsim) {
+               y <- list()
+               for (i in seq(nsim))
+                 y[[i]] <- signal + sigma * rnorm(n)
+               return(y)
+             }))
+}
+
+test_that("generate_model works with ... when vary_params = NULL", {
+  dir <- file.path(tempdir(), "example")
+  if (!dir.exists(dir)) dir.create(dir)
+  expect_error(generate_model(make_regmodel, dir = dir), "missing")
+  expect_error(generate_model(make_regmodel, dir = dir, n = 5, p = 2),
+               "missing")
+  expect_error(not(generate_model(make_regmodel, dir = dir, n = 5, p = 2,
+                                  sigma = 2)))
+  expect_error(generate_model(make_regmodel, dir = dir, n = 5, p = 2,
+                              sigma = 2), "sets n to a value different from")
+  expect_error(not(generate_model(make_regmodel, dir = dir, n = 1, p = 2, sigma = 2)))
+  unlink(dir, recursive = TRUE)
+})
+
+test_that("generate_model works when vary_params in non-NULL", {
+  dir <- file.path(tempdir(), "example")
+  if (!dir.exists(dir)) dir.create(dir)
+  expect_error(generate_model(make_regmodel, dir = dir, vary_along = "n"),
+               "vary_along")
+  expect_error(generate_model(make_regmodel, dir = dir, vary_along = "n",
+                              n = 2, p = 4), "list")
+  expect_error(generate_model(make_regmodel, dir = dir, vary_along = "n",
+                              n = as.list(c(2,4)), p = 4), "sigma")
+  expect_error(not(generate_model(make_regmodel2, dir = dir, vary_along = "n",
+                                  n = as.list(c(2,4)), p = 4, sigma = 1)))
+  expect_error(generate_model(make_regmodel2, dir = dir, vary_along = "n",
+                              n = as.list(c(2,4)), p = as.list(c(2, 10)),
+                              sigma = 1), "non-numeric argument")
+  generate_model(make_regmodel2, dir = dir, vary_along = c("n", "p"),
+                              n = as.list(c(2,4)), p = as.list(c(2, 3, 10)),
+                              sigma = 1)
+  model1 <- load_model(dir = dir, "reg2/n1/p3")
+  generate_model(make_regmodel2, dir = dir, n = 2, p = 10, sigma = 1)
+  model2 <- load_model(dir = dir, "reg2")
+  expect_identical(model1@params, model2@params)
+  unlink(dir, recursive = TRUE)
+})
