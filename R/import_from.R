bd1ec85dd1a362c596e312a9950ff163e395d92d
jbien <jacob.bien@gmail.com>
2016-06-22 18:31:43 -0400

    importFrom as suggested by R CMD check

--- R/import_from.R
+++ R/import_from.R
@@ -6,8 +6,13 @@
 #' @keywords internal
 NULL
 
-
-
-
-#' @importFrom methods setClass setGeneric setMethod
+#' @importFrom methods setClass setGeneric setMethod as
+#' callNextMethod new validObject
+#' @importFrom grDevices palette
+#' @importFrom graphics axis boxplot legend lines par plot points segments text
+#' title
+#' @importFrom stats as.formula loess predict sd
+#' @importFrom utils installed.packages
+#' @importFrom parallel makePSOCKcluster clusterExport parLapplyLB stopCluster
+#' clusterExport clusterEvalQ
 NULL
