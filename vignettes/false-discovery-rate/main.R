7b956ef219f4690175a747b0285bb24f46cadb5b
jbien <jacob.bien@gmail.com>
2016-07-03 03:31:52 -0400

    reduce computation time for fdr vignette (smaller n and smaller nsim)

--- vignettes/false-discovery-rate/main.R
+++ vignettes/false-discovery-rate/main.R
@@ -15,18 +15,18 @@ name_of_simulation <- "fdr"
 sim <- new_simulation(name = name_of_simulation,
                       label = "False Discovery Rate") %>%
   generate_model(make_correlated_pvalues, seed = 123,
-                 n = 100,
+                 n = 20,
                  pi0 = list(0.8, 1),
                  rho = list(-0.01, 0, 0.1, 0.9),
                  vary_along = c("pi0", "rho")) %>%
-  simulate_from_model(nsim = 50, index = 1:4) %>%
+  simulate_from_model(nsim = 25, index = 1:4) %>%
   run_method(bh_methods, parallel = list(socket_names = 2)) %>%
   evaluate(list(fdp, nd))
 
 ## @knitr main2
 
 sim <- sim %>%
-  simulate_from_model(nsim = 50, index = 5:8) %>%
+  simulate_from_model(nsim = 25, index = 5:8) %>%
   run_method(bh_methods, parallel = list(socket_names = 2)) %>%
   evaluate(list(fdp, nd))
 
@@ -35,7 +35,7 @@ sim <- sim %>%
 sim2 <- subset_simulation(sim, pi0 == 1 & rho == -0.01) %>%
   rename("fdr-negdep") %>%
   relabel("BH Procedure under negative dependence") %>%
-  simulate_from_model(nsim = 50, index = 9:20) %>%
-  run_method(bh_methods) %>%
+  simulate_from_model(nsim = 25, index = 9:20) %>%
+  run_method(bh_methods, parallel = list(socket_names = 2)) %>%
   evaluate(list(fdp, nd))
 
